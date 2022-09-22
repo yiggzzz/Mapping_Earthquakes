e8908a4805c8c2f5a2fce7e633fa948fb505da89
yiggzzz <yvarillas7@gmail.com>
2022-09-22 19:23:07 -0400

    Editing logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -17,6 +17,23 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // This method is useful when we need to add multiple tile layers, 
 // or a background image of our map(s), which we will do later in this module.
 
+// Add a marker to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// Another way to do it
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Another way to do it 
+// Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
